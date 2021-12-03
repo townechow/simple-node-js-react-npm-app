0aa558f3390f0099fc0f6d77b448a3b7669f4300
datang <you@example.com>
2021-12-03 11:51:40 +0000

    test

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          dadadadadadadateang
+          docker run -d  --rm   -u root   -p 8080:8080   -v jenkins-data:/var/jenkins_home   -v /var/run/docke
+          r.sock:/var/run/docker.sock   -v "$HOME":/home   jenkinsci/blueocean
         </p>
       </div>
     );
